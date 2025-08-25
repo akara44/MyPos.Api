9bc683f771c5ed56dd2f1a61f91a97baf2e9f3f0
akara44 <ahmetkara2044@gmail.com>
2025-08-25 15:00:51 +0300

    implement split payment endpoint and validation

---
+++ MyPos.Infrastructure/Migrations/20250825115603_SplitPayment.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyPos.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SplitPayment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentTypeName",
+                table: "Payments",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "SaleId",
+                table: "Payments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_SaleId",
+                table: "Payments",
+                column: "SaleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Payments_Sales_SaleId",
+                table: "Payments",
+                column: "SaleId",
+                principalTable: "Sales",
+                principalColumn: "SaleId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Payments_Sales_SaleId",
+                table: "Payments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Payments_SaleId",
+                table: "Payments");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentTypeName",
+                table: "Payments");
+
+            migrationBuilder.DropColumn(
+                name: "SaleId",
+                table: "Payments");
+        }
+    }
+}
